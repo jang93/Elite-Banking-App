9076394fe6afbb56d62dfe1ccaa946334a47e283
Joel Ang <jang93@gmail.com>
2015-06-22 10:02:04 +0000

    Changes from Screen Builder

--- testtest/authenticationView2/index.js
+++ testtest/authenticationView2/index.js
@@ -5,10 +5,11 @@ app.authenticationView2 = kendo.observable({
 });
 (function(parent) {
     var provider = app.data.testBackend,
-
+        mode = 'signin',
+        registerRedirect = 'home',
         signinRedirect = 'homeView',
         init = function() {
-            var activeView = '.signin-view';
+            var activeView = mode === 'signin' ? '.signin-view' : '.signup-view';
 
             if (provider.setup && provider.setup.offlineStorage && !app.isOnline()) {
                 $('.offline').show().siblings().hide();
@@ -17,7 +18,7 @@ app.authenticationView2 = kendo.observable({
             }
         },
         successHandler = function(data) {
-            var redirect = signinRedirect;
+            var redirect = mode === 'signin' ? signinRedirect : registerRedirect;
 
             if (data && data.result) {
                 app.user = data.result;
@@ -35,6 +36,21 @@ app.authenticationView2 = kendo.observable({
                     password = authenticationView2Model.password;
 
                 provider.Users.login(email, password, successHandler, init);
+            },
+            register: function() {
+                var email = authenticationView2Model.email.toLowerCase(),
+                    password = authenticationView2Model.password,
+                    displayName = authenticationView2Model.displayName,
+                    attrs = {
+                        Email: email,
+                        DisplayName: displayName
+                    };
+
+                provider.Users.register(email, password, attrs, successHandler, init);
+            },
+            toggleView: function() {
+                mode = mode === 'signin' ? 'register' : 'signin';
+                init();
             }
         });
 
