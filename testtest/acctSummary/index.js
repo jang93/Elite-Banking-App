5474f474a01a04ff27c9cd3e8bf05f067a7104f0
Joel Ang <jang93@gmail.com>
2015-06-22 11:54:33 +0000

    Changes from Screen Builder

--- testtest/acctSummary/index.js
+++ testtest/acctSummary/index.js
@@ -3,3 +3,10 @@
 app.acctSummary = kendo.observable({
     onShow: function() {}
 });
+(function(parent) {
+    var acctSummaryModel = kendo.observable({
+        fields: {}
+    });
+
+    parent.set('acctSummaryModel', acctSummaryModel);
+})(app.acctSummary);
